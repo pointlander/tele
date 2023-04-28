46123430f64287890042e01f41b8acfbdbcf4057
Andrew Snodgrass <git@pointlander.info>
2023-04-28 17:01:05 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pointlander/tele
+
+go 1.18
+
+require github.com/itsubaki/q v0.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,45 @@
+// Copyright 2023 The Tele Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/itsubaki/q"
+)
+
+func main() {
+	qsim := q.New()
+	q0 := qsim.Zero()
+
+	// Define the parameters
+	k := 1.0
+	h := 1.5
+
+	// Prepare the ground state
+	alpha := -math.Asin((1 / math.Sqrt(2)) * (math.Sqrt(1 + h/math.Sqrt(h*h+k*k))))
+
+	qsim.RY(2*alpha, q0)
+	qsim.I(q0)
+	fmt.Println("2*alpha=", 2*alpha)
+
+	sin := func(k, h float64) float64 {
+		a := h*h + 2*k*k
+		b := h * k
+		return b / math.Sqrt(a*a+b*b)
+	}
+	phi := 0.5 * math.Asin(sin(k, h))
+
+	qsim.RY(-2*phi, q0)
+	fmt.Println("2*phi=", 2*phi)
+
+	qsim.H(q0)
+	qsim.Measure(q0)
+
+	for _, s := range qsim.State() {
+		fmt.Println(s)
+	}
+}
